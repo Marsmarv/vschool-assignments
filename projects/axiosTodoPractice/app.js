b0ebc205d0b96624ab09ff3d0802603fcebe5469
Marvin Romero <marvin.r02@gmail.com>
2019-05-10 14:45:51 -0600

    zen garden push

--- projects/axiosTodoPractice/app.js
+++ projects/axiosTodoPractice/app.js
@@ -2,56 +2,102 @@ let form = document.addTodo
 let todoList = []
 
 axios.get('https://api.vschool.io/marv/todo/').then((response)=>{
+    todoList = response.data
     displayToDo(response.data)
 })
-    function displayToDo(todos){
-        todos.forEach(todo => {
-            let parentDiv = document.createElement('div')
-            let titleH2 = document.createElement('h2')
-            let description = document.createElement('p')
-            let price = document.createElement('p')
-            let imgURL = document.createElement('img')
 
-            titleH2.innerText = todo.title
-            description.innerText = todo.description
-            price.innterText = todo.price
-            imgURL.innterText = todo.imgURL
+function displayToDo(todos, updated){
+    if(updated){
+        document.getElementById('container').innerHTML = ''
+    }
 
-            parentDiv.appendChild(titleH2)
-            parentDiv.appendChild(description)
-            parentDiv.appendChild(price)
-            parentDiv.appendChild(imgURL)
-            document.getElementById('container').appendChild(parentDiv)
-            
-            if(todo.completed === true){
-                titleH2.style.textDecoration = "line-through"
-                description.style.textDecoration = "line-through"
+    todos.forEach(todo => {
+        let parentDiv = document.createElement('div');
+        let titleH2 = document.createElement('h2');
+        let description = document.createElement('p');
+        let price = document.createElement('p');
+        let imgUrl = document.createElement('img');
+        let delBtn = document.createElement('button')
+        let checkBox = document.createElement('input')
+        
+        imgUrl.style.height = "100px"
+
+        parentDiv.className = 'todoBox'
+        checkBox.setAttribute('type', 'checkbox')
+
+        titleH2.innerText = todo.title;
+        description.innerText = todo.description;
+        price.innerText = `$${todo.price}`;
+        imgUrl.src = todo.imgUrl;
+        delBtn.innerText = 'delete'
+
+        delBtn.addEventListener('click', function(){
+            parentDiv.style.display = 'none';
+            axios.delete(`https://api.vschool.io/marv/todo/${todo._id}`);
+        })
+
+        checkBox.addEventListener('click', function(e){
+            e.preventDefault()
+            checkTodo(todo)
+            axios.get('https://api.vschool.io/marv/todo/').then(res => {
+                todoList = res.data
+            })
+        })
+        
+        if(todo.completed){
+            checkBox.checked = true
+            parentDiv.style.textDecoration = 'line-through'
+        } else {
+            parentDiv.style.textDecoration = 'none'
+        }
+
+        parentDiv.appendChild(titleH2);
+        parentDiv.appendChild(description);
+        parentDiv.appendChild(price);
+        parentDiv.appendChild(imgUrl);
+        parentDiv.appendChild(delBtn)
+        parentDiv.appendChild(checkBox)
+        
+        document.getElementById('container').appendChild(parentDiv)
+    
+    })
+}
+
+const checkTodo = oldTodo => {
+    axios.put(`https://api.vschool.io/marv/todo/${oldTodo._id}`, {completed: !oldTodo.completed}).then(response => {
+        const newList = todoList.map(todo => {
+            if(todo._id === oldTodo._id){
+                return response.data
+            } else {
+                return todo
             }
         })
-    }
+        displayToDo(newList, true)
+    })
+}
 
     form.addEventListener('submit', function(event) {
         event.preventDefault()
-        let newTitle = form.title.value
-        let newDescription = form.description.value
-        let newPrice = form.price.value
-        let newImg = form.imgURL.value
+        let newTitle = form.title.value;
+        let newDescription = form.description.value;
+        let newPrice = form.price.value;
+        let newImg = form.imgUrl.value;
 
         let newObject = {
             title: newTitle,
             description: newDescription,
             price: newPrice,
-            imgURL: newImg
+            imgUrl: newImg
         }
-        postTodo(newObject)
+        postTodo(newObject);
     })
+
 
     const postTodo = (newTodo) => {
         axios.post('https://api.vschool.io/marv/todo/', newTodo).then((response)=>{
-            todoList.push(response.data)
-            displayToDo(todoList)
+            todoList.push(response.data);
+            displayToDo([response.data]);
         })
     }
-
 // localStorage.setItem('isAuth', true)
 // localStorage.setItem('isAuth')
