abf3e5e16a7a9cf1e6d38655f1719def2e52eb73
Marvin Romero <marvin.r02@gmail.com>
2019-05-13 22:46:35 -0600

    new React push

---
+++ exercises/in class/react-intro/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// first argunment is the App component
+// second argument is the location of root
+
