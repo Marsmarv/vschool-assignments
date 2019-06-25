ad6d41b68e099215533014b8f05ed19db1684c38
Marvin Romero <marvin.r02@gmail.com>
2019-06-25 00:50:37 -0600

    port

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,11 +4,12 @@ import Resume from './Resume'
 import Contact from './Contact'
 import Myself from './Myself'
 import './styles.css'
-
+import ProgressBar from "react-scroll-progress-bar";
 
 const App = () => {
   return (
     <>
+    <ProgressBar bgcolor="#E5960B"/>
     <Projects/>
     <Resume/>
     <Contact/>
--- portfolio/src/Resume.js
+++ portfolio/src/Resume.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Fade from 'react-reveal/Fade'
 import ModalImage from 'react-modal-image'
 import resFront from './resFront.png'
 
 const Resume = () => {
+  const [showRes, setShowRes] = useState(false)
+  const modalDisplay = showRes ? "show-modal" : "no-display"
+  const modalDisplayTwo = showRes ? "xbox" : "no-display"
+
   return (
     <Fade bottom delay={500}>
       <div>
         <div className="res">
-
           <div className="resBox">
             <Fade bottom cascade delay={3000}>
               <h3>Résumé</h3>
-              {/* <div className="pic"></div> */}
-              <div className='pic'>
-              <ModalImage 
-                small={resFront}
-                medium={resFront}
-                alt="Marvin Romero"
-              />
-              </div>
+              <div className='pic' onClick={() => setShowRes(!showRes)}></div>
+              <Fade bottom opposite when={showRes === true}>
+                <div className={modalDisplay} onClick={() => setShowRes(!showRes)}>
+                  <div className={modalDisplayTwo}>d</div>
+                </div>
+              </Fade>
             </Fade>
           </div>
-
         </div>
       </div>
     </Fade>
