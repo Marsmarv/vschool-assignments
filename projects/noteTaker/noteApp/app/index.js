2188475f77eb82d21cb5acb0b8f5571f5df31e9f
Marvin Romero <marvin.r02@gmail.com>
2019-11-07 18:05:13 -0500

    added info

---
+++ projects/noteTaker/noteApp/app/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Home from './components/Home/Home.component'
+
+class NoteApp extends Component {
+  render(){
+    return(
+      <Home />
+    );
+  }
+}
+
+export default NoteApp
