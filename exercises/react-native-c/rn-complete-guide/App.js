ffb453d0976793609ba6a730070b6a34d2a2b65c
Marvin Romero <marvin.r02@gmail.com>
2019-12-05 00:32:26 -0500

    pushing list where the entered goal hook isn't an object

--- exercises/react-native-c/rn-complete-guide/App.js
+++ exercises/react-native-c/rn-complete-guide/App.js
@@ -1,46 +1,60 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const {container, goalContainer, listItem, textContainer}  = styles
+  //destructuring the styles out of each styleSheet object
+
   const [enteredGoal, setEnteredGoal] = useState('')
   const [courseGoals, setCourseGoals] = useState([])
-  const goalInputHandler = enteredText => {
-    setEnteredGoal(enteredText)
-  }
-  const addGoalHandler = () => {
-    // setCourseGoals([...courseGoals, enteredGoal]) ; this works fine but 
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]) //gauranteed to work because react gives you the gauranteed latest state snapshot before applying state change
-  }
+
+  const goalInputHandler = enteredText => setEnteredGoal(enteredText)
+  const addGoalHandler = () => setCourseGoals(currentGoals => [...currentGoals, enteredGoal]) 
+  //gauranteed to work because react gives you the gauranteed latest state snapshot before applying state change
+  // setCourseGoals([...courseGoals, enteredGoal]) ; this works fine though 
+
   return (
-    <View style={styles.container}>
-      <View style={styles.goalContainer}>
+    <View style={ container }>
+      <View style={ goalContainer }>
         <TextInput 
           placeholder="Course Goal" 
-          style={styles.textContainer} 
-          onChangeText={goalInputHandler}
+          style={ textContainer } 
+          onChangeText={ goalInputHandler }
         />
-        <Button title="ADD" onPress={addGoalHandler}/>
+        <Button title="ADD" onPress={ addGoalHandler }/>
       </View>
-      <View>
-        {courseGoals.map(goal => <Text>{goal}</Text>)}
-      </View>
+      <FlatList 
+        data={ courseGoals } 
+        renderItem={ itemData => (
+          <View style={ listItem }>
+            <Text>{ itemData.item }</Text>
+          </View>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     padding: 50
   },
-  textContainer:{
+  textContainer: {
     borderColor: 'black',
     borderWidth: 1,
     padding: 10,
     width: "80%"
   },
-  goalContainer:{
+  goalContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center"
+  },
+  listItem: {
+    padding: 10,
+    borderWidth: 1,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
   }
 });
