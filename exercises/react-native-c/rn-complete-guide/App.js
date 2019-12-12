d994ee0afb8bb45d4dda90c9d4aab847a6fd9be6
Marvin Romero <marvin.r02@gmail.com>
2019-12-11 21:29:15 -0500

    changes

--- exercises/react-native-c/rn-complete-guide/App.js
+++ exercises/react-native-c/rn-complete-guide/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, FlatList, Button } from 'react-native';
-
 import GoalItem from './components/GoalItem'
 import GoalInput from './components/GoalInput'
 
@@ -8,33 +7,31 @@ export default function App() {
   const {container}  = styles
   const [courseGoals, setCourseGoals] = useState([])
   const [addModal , setAddModal] = useState(false)
+
   const addGoalHandler = goalTitle => {
+    if (goalTitle.length === 0) {return}
     setCourseGoals(currentGoals => [
       ...currentGoals, 
       { id: Math.random().toString(), value: goalTitle }
     ]) 
     setAddModal(false)
   }
-  // setCourseGoals( [...courseGoals, enteredGoal] ) ;; this would work as well.
   const delGoalHandler = goalId => {
     setCourseGoals(currentGoals => {
       return currentGoals.filter(goal => goal.id !== goalId)
     })
   }
-
-  const cancelGoalHandler = () => {
-    setAddModal(false)
-  }
+  const cancelGoalHandler = () => setAddModal(false)
 
   return (
     <View style={ container }>
       <Button title="Add New Goal" onPress={() => setAddModal(!addModal)}/>
-      <GoalInput 
+      <GoalInput
         visible={addModal} 
         AddGoal={addGoalHandler} 
         cancel={cancelGoalHandler}
       />
-      <FlatList       
+      <FlatList
         keyExtractor={(item, index) => item.id}
         data={ courseGoals } 
         renderItem={ itemData => (
--- exercises/react-native-c/rn-complete-guide/components/GoalInput.js
+++ exercises/react-native-c/rn-complete-guide/components/GoalInput.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = props => {
-  const {goalContainer, textContainer} = styles
+  const {goalContainer, textContainer, buttonContainer, button} = styles
   const {visible, cancel} = props
   const [enteredGoal, setEnteredGoal] = useState('')
   const goalInputHandler = enteredText => setEnteredGoal(enteredText)
@@ -19,10 +19,16 @@ const GoalInput = props => {
           style={ textContainer } 
           onChangeText={ goalInputHandler }
         />
-        <Button title="CANCEL" color="red" onPress={ cancel } />
-        <Button title="ADD" onPress={ addGoalHandler }/>
-        {/* <Button title="ADD" onPress={ props.AddGoal.bind(this, enteredGoal) }/> */}
-        {/* alternatively but i don't like how this looks. */}
+        <View style={buttonContainer}>
+          <View style={button}>
+          <Button title="CANCEL" color="red" onPress={ cancel } />
+          </View>
+          
+          <View style={button}>
+          <Button title="ADD" onPress={ addGoalHandler }/>
+          </View>
+        </View>
+
       </View>
     </Modal>
   )
@@ -40,6 +46,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '60%',
+  },
+  button: {
+    width: "60%",
   }
 })
 
