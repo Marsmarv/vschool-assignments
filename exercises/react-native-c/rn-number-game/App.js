03a0306602e72e4d2498f75698ac789a7edcf0b3
Marvin Romero <marvin.r02@gmail.com>
2020-02-01 14:42:45 -0500

    added components

--- exercises/react-native-c/rn-number-game/App.js
+++ exercises/react-native-c/rn-number-game/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header'
+import StartGameScreenName from './screens/StartGameScreen'
 
 export default function App() {
   return (
-    <View>
-
+    <View style={styles.screen}>
+      <Header title="guess a numero"></Header>
+      <StartGameScreenName/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1
+  }
 });
