36eacdd3e82eeffd00e2d443fc9004db2af511b0
Marvin Romero <marvin.r02@gmail.com>
2020-02-08 19:22:54 -0500

    first test with mocha

---
+++ exercises/unitTesting/classroom-test/src/tests/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe( "testing Main", () => {
+  context("function to be tested", () => {
+    it("should do something", () => {
+      assert.equal(1,1)
+    })
+  })
+})
