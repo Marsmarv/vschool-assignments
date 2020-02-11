e4fe0718dd62bb3b8586ed300c783c421b96e38a
Marvin Romero <marvin.r02@gmail.com>
2020-02-11 18:57:44 -0500

    student data up

--- exercises/unitTesting/classroom-test/src/App.js
+++ exercises/unitTesting/classroom-test/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       
       {/* <Room /> */}
       {/* <Desk /> */}
-      {/* <Student /> */}
+      <Student />
       {/* <Daily /> */}
     </div>
   )
--- exercises/unitTesting/classroom-test/src/components/Student.js
+++ exercises/unitTesting/classroom-test/src/components/Student.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import studentData from '../data/student.json'
 let students = studentData
 
 const Student = () => {
+  const [student, setStudent] = useState()
+  const handleChange = e => {
+    const {value} = e.target
+    setStudent(value)
+  }
   return(
+    <>
     <div className="student">
+      <div>
+        <select onChange={handleChange}name="student" value={student}>
+          <option value="" disabled selected hidden>Please Choose Student...</option>
+          {students.map(student => {
+            return (<><option value={student.id}>{student.id}</option></>)
+          })}
+        </select>
+
+        <br/><br/>
+
+        <div>
+          {student && 
+            <>
+              {students.map((studentInfo, i)=>{
+                return studentInfo.id.toString() === student &&
+                <>
+                  <div>
+                    <u>STUDENT BIO {i + 1}</u>
+                      <div> Student id: {studentInfo.id}</div>
+                      <div>Given Name: {studentInfo.bio.givenName}</div>
+                      <div>Family Name: {studentInfo.bio.familyName}</div>
+                      <div>NickName: {studentInfo.bio.nickName}</div>
+                      <div>Email: {studentInfo.bio.email}</div>
+                      <div>Age: {studentInfo.bio.age}</div>
+                      <div>Grade: {studentInfo.bio.grade}</div>
+                    <br/>
+                    student history: 
+                      <div>abscences: {studentInfo.history.absences}</div>
+                      <div>GPA: {studentInfo.history.gpa}</div>
+                    <br/>
+                    student grades: 
+                      <div>Project: {studentInfo.grades.project1}</div>
+                      <div>Project: {studentInfo.grades.project2}</div>
+                      <div>Project: {studentInfo.grades.project3}</div>
+                      <div>Project: {studentInfo.grades.project4}</div>
+                    <br/>
+                  </div>
+                </>
+              })}
+            </>
+          }
+        </div>
+      </div>
+
+    </div>
+
+
+
+
+
+    {/* <div className="student">
       {students.map((student, i)=>{
         return (
           <div>
@@ -30,7 +87,10 @@ const Student = () => {
           </div>
         )
       })}
-    </div>
+    </div> */}
+
+    
+    </>
   )
 }
 
