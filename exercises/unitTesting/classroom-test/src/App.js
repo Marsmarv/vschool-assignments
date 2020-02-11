508fb5e90b78f0842ed4b6f47dde0a87d5043810
Marvin Romero <marvin.r02@gmail.com>
2020-02-10 19:55:21 -0500

    not much front end laid out

--- exercises/unitTesting/classroom-test/src/App.js
+++ exercises/unitTesting/classroom-test/src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Room from './components/Room'
 import Desk from './components/Desk'
 import Student from './components/Student'
+import './components/styles.css'
 
 const App = () => {
   return (
-    <>
+    <div className="main-container">
       <Room />
-      <br/>
       <Desk />
-      <br/>
       <Student />
-    </>
+    </div>
   )
 }
 
