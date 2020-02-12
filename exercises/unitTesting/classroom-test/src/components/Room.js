08e099051017df61729ad1c1c1a6d0cc383bed06
Marvin Romero <marvin.r02@gmail.com>
2020-02-12 00:01:09 -0500

    room position info on click

--- exercises/unitTesting/classroom-test/src/components/Room.js
+++ exercises/unitTesting/classroom-test/src/components/Room.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
 import roomData from '../data/room.json'
-import Daily from './Daily'
+import Daily from '../data/daily.json'
 let room = roomData[0]
 
 
 const Room = () => {
-  const [chosenDesk, setChosenDesk] = useState()
+  const [deskChosen, setDeskChosen] = useState()
+  const [modal, setModal] = useState()
+  const modalDisplay = modal ? "show-modal" : "no-display"
   return(
     <>
-    <div className="room">
-      <div><b>room id:</b> {room.roomId}</div><br/>
-      <div className="position-container">
-        <u>room positions</u>
-        <div className="positions" >
-          {room.positions.sort().map( roomId => { 
-            return (<><div className="desks" onClick={()=>{setChosenDesk(roomId)}}>id: { roomId }</div></>)
-          })}
+      <div className="room">
+        <div><b>room id:</b> {room.roomId}</div>
+        <div className="position-container">
+         <h3><u>room positions</u></h3>
+         <button onClick={()=>{setModal(false)}}>clear room positions info</button>
+          <div className="positions" >
+            {room.positions.sort().map( roomId => { 
+              return (<><div className="desks" onClick={()=>{setDeskChosen(roomId); setModal(true)}}>id: { roomId }</div></>)
+            })}
+          </div>
         </div>
       </div>
-    </div>
+
+      <div>
+        {deskChosen && 
+          <div className="modal-container">
+            {Daily.map( deskChosenInfo => {
+              return deskChosenInfo.desks.map(deskEntry => {
+                return deskChosen === deskEntry.positionId &&
+                <div className={modalDisplay}>
+                  <div className="modal-info">date:{deskChosenInfo.date}</div>
+                  <div className="modal-info">Entry Id: {deskEntry.entryId}</div>
+                  <div className="modal-info">Desk Id: {deskEntry.deskId}</div>
+                  <div className="modal-info">Status: {deskEntry.status}</div>
+                </div>
+              })
+            })}
+          </div>
+        }
+      </div>
     </>
   )
 }
