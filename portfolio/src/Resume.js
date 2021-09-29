f74f7b8038b7b76be847e860420fc079fca9c3a0
marsmarv <marvin.r02@gmail.com>
2021-09-29 19:33:20 -0400

    updating res

--- portfolio/src/Resume.js
+++ portfolio/src/Resume.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import Fade from 'react-reveal/Fade'
-import resume from './images/MarvFEres.pdf'
+import resume from './images/MarvFEresWc.pdf'
 
 const Resume = () => {
   const [showRes, setShowRes] = useState(false)
